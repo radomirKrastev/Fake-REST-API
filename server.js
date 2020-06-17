e257c81a21a2a9c80cc84ff0a9ebf8289988d49b
radomirKrastev <49518363+radomirKrastev@users.noreply.github.com>
2020-06-17 20:02:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
-function killHeroku() {
-    process.exit(0);
+// function killHeroku() {
+    // process.exit(0);
 
     server.use(middlewares);
     server.use(router);
         
     server.listen(port);
-    
-    setTimeout(killHeroku, 720000);
-}
 
-killHeroku();
+//     setTimeout(killHeroku, 720000);
+// }
+
+// killHeroku();
