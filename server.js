ed160b79746c366933b6c3b9913f6fec079af99d
radomirKrastev <49518363+radomirKrastev@users.noreply.github.com>
2020-06-18 11:42:17 +0300

    add expiration note

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 const Heroku = require('heroku-client');
+//note: This token expires after a year
 const token = 'c475269b-6085-4db9-9601-22f79f8ed83a';
 const appName = 'protected-chamber-82435';
 
